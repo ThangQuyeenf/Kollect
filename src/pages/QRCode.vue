756cce3696b44020a35fff3c199e8cf558da326c
thangdq <thangquyen2018@gmail.com>
2023-11-13 00:15:41 +0700

    update QRCode

--- src/pages/QRCode.vue
+++ src/pages/QRCode.vue
@@ -20,29 +20,27 @@ export default {
     },
     methods: {
         async scanCode() {
-            liff.init({ liffId: '2001602140-y3gZ0PPj' })
-                .then(async () => {
-                    await liff.login()
-                    liff.scanCodeV2()
-                        .then(result => {
-                            // Process the result, which contains the QR code data
-                            const qrCodeData = result.value;
-                            this.scanResult = qrCodeData;
-                            this.isScanResult = true
-                            console.log("Scanned QR code data: " + qrCodeData);
-                        })
-                        .catch(error => {
-                            // Handle any errors that occur during the scanning process
-                            alert(error)
-                            console.error("Error scanning QR code: " + error);
-                        });
-
+            liff.scanCodeV2()
+                .then(result => {
+                    // Process the result, which contains the QR code data
+                    const qrCodeData = result.value;
+                    this.scanResult = qrCodeData;
+                    this.isScanResult = true
+                    console.log("Scanned QR code data: " + qrCodeData);
                 })
-                .catch((err) => {
-                    alert('ERROR: ', err)
+                .catch(error => {
+                    // Handle any errors that occur during the scanning process
+                    alert(error)
+                    console.error("Error scanning QR code: " + error);
                 });
         }
     },
+    async created() {
+        liff.init({ liffId: '2001602140-y3gZ0PPj' })
+        if(!liff.isLoggedIn()){
+            await liff.login()
+        }
+    }
 };
 </script>
   
