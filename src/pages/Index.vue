4f3b333b1c76928e1aef8f28894e7c7317e0750e
thangdq <thangquyen2018@gmail.com>
2023-11-12 23:42:20 +0700

    update index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -36,6 +36,7 @@ export default {
             await liff.init({ liffId: '2001602140-y3gZ0PPj' })
             const queryString = decodeURIComponent(window.location.search)
             const params = new URLSearchParams(queryString)
+            alert (params)
             if (params.get('param') !== null) {
                 alert(params)
                 this.data = params.get('param')
